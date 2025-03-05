368b1514c680bc5e89f53e72955ca969da8b8b86
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-05 15:39:10 -0800

    Figured it out

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [bottomBg, setbottomBg] = useState('bg-[#1e202a] h-full w-full -z-10 relative')
+  const [topBg, setTopBg] = useState('bg-[#1f212e] h-[30%] w-full absolute top-0 ')
 
 
   return (
     <>
      <div className='w-screen h-screen flex flex-col relative'>
       {/* Background */}
-      <div className='bg-[#1e202a] h-full w-full -z-10 relative'>
-        <div className='bg-[#1f212e] h-[30%] w-full absolute top-0 '> 
+      <div className={bottomBg}>
+        <div className={topBg}> 
         </div>
       </div>
+
+
+      
 
      </div>
     </>
